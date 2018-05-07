7958784834276ec50cb8d4d6bf130b606ad528cb
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-05-06 19:20:09 -0700

    more to index.html, some javascript variables declared

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//Global Variables
+//================================================================================
+var wordArray = ["Mega Man", "Super Mario Bros", "Castlevania", "Metroid", "Bionic Commando"];
+var lettersGuessed = [];
+var wins = 0;
+var guessesRemaining = 6;
+
+
+
+//Functions
+//================================================================================
+    document.onkeyup = function(event) {
+      var key = event.key;
+    }
+
+//Main Process
+//================================================================================
+  
